5f2e4d2e23f9522435f94a278293279e0e286892
Shohel Rana <shohelrrrana@gmail.com>
2023-12-28 14:57:04 +0600

    implement database config

---
+++ src/config/datasource.config.ts
@@ -0,0 +1,18 @@
+import path from 'path'
+import { DataSource } from 'typeorm'
+
+const databaseType: any     = process.env.DATABASE_TYPE
+const databaseUrl           = process.env.DATABASE_URL
+const databaseSync: boolean = Number( process.env.DATABASE_SYNC ) === 1
+
+export const appDataSource = new DataSource( {
+    type: databaseType,
+    url: databaseUrl,
+    ssl: { rejectUnauthorized: false },
+    synchronize: databaseSync,
+    logging: false,
+    migrationsRun: false,
+    entities: [
+        path.join( __dirname, '/../api/v1/entities/**/*.{ts,js}' )
+    ]
+} )
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { httpServer } from '@/config/server.config'
+import { appDataSource } from '@/config/datasource.config'
 
+//store port
 const PORT = process.env.PORT || 4000
 
 //run the server
 httpServer.listen(PORT, () => {
     console.log(`🚀 Server listening on port ${PORT}`)
     console.log(`======= ENV: ${process.env.NODE_ENV} =======`)
+
+    async function initializeDatasource() {
+        try {
+            //initialize database connection
+            await appDataSource.initialize()
+            console.log('Data Source has been initialized!')
+        } catch (err) {
+            console.error('Error during Data Source initialization', err)
+        }
+    }
+
+    //kick datasource
+    initializeDatasource()
 })
